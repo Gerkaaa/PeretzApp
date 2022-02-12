b24295bb40bef09e398bbea6905935a4f8802815
Gerkaaa <gera2.01@mail.ru>
2022-02-12 20:06:45 +0300

    Initial Commit

---
+++ PeretzApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PeretzApp
+//
+//  Created by Герекмаз Газимагомедова on 12.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
