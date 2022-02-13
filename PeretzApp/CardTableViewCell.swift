e29d73a11909b94456716385d85b463be42be7f7
Gerkaaa <gera2.10@mail.ru>
2022-02-13 21:39:39 +0300

    feature: products list layout

---
+++ PeretzApp/CardTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CardTableViewCell.swift
+//  PeretzApp
+//
+//  Created by Герекмаз Газимагомедова on 12.02.2022.
+//
+
+import UIKit
+
+class CardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- PeretzApp/ViewController.swift
+++ PeretzApp/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let idCell = "cardCell"
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: idCell) as! CardTableViewCell
+
+        return cell
+    }
+    
+//            func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//                return 100.0
+//            }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print("\(indexPath.row)")
+//    }
+}
